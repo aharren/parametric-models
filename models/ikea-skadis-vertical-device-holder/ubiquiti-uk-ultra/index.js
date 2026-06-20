24c74aa3c168e6f08d3a2a074bb382c67b930022
Arne Harren <ah@0xc0.de>
2026-06-20 15:33:46 +0200

    in the vertical device holder model for Ubiquiti UK Ultra, add a hole for the device LED

--- models/ikea-skadis-vertical-device-holder/ubiquiti-uk-ultra/index.js
+++ models/ikea-skadis-vertical-device-holder/ubiquiti-uk-ultra/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const { cylinder } = require('@jscad/modeling').primitives;
+const { subtract } = require('@jscad/modeling').booleans;
+const { rotateX, align } = require('@jscad/modeling').transforms;
+
+const measurements = require('../../../lib/measurements');
+
 // Ubiquiti UK Ultra / Swiss Army Knife Access Point
 const config = {
   deviceHeight: 33.2,
@@ -13,6 +19,25 @@ const config = {
   numBracketsY: 2,
   bracketWidth: 2,
   bracketDepth: 2,
+
+  deviceDepthLed: 22,
+  deviceLedHoleDiameter: 10,
+};
+
+config.shellModifier = (object) => {
+  const boundingBox = measurements.measureBoundingBox(object);
+  const frontBottom = measurements.pointInBoundingBox({ modes: ['center', 'min', 'min'] }, boundingBox);
+  const ledHolePosition = [
+    frontBottom[0],
+    frontBottom[1],
+    frontBottom[2] + config.wallThickness + config.deviceDepthLed
+  ]
+  const ledHole = align({ modes: ['center', 'min', 'center'], relativeTo: ledHolePosition },
+    rotateX(Math.PI / 2,
+      cylinder({ height: config.wallThickness + config.bracketDepth, radius: config.deviceLedHoleDiameter / 2, segments: 256 })
+    )
+  );
+  return subtract(object, ledHole);
 };
 
 module.exports = require('../../../lib/submodel').exports(require('../ikea-skadis-vertical-device-holder'), config);

6db949e7a4b63ca68f287dc9d14757019e4641e8
Arne Harren <ah@0xc0.de>
2026-07-04 14:47:31 +0200

    add a variant of the 57/50 suction hose adapter with a mounting plate

---
+++ models/suction-hose-adapter/generic-connecting-sleeve-o57-I53-generic-hose-o55-i50-mounting-plate/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { degToRad } = require('@jscad/modeling/src').utils;
+const { union } = require('@jscad/modeling').booleans;
+
+const connectors = require('../../lib/suction-hose-connectors');
+const mounts = require('../../lib/mounting-plates');
+
+const config = {
+  connector1: connectors.invert({ play: 0.0 }, connectors.library.generic.ConnectingSleeve_O57_I53.plug),
+  connector2: connectors.invert({ play: 0.0 }, connectors.library.generic.Hose_O55_I50.socket),
+  bendAngle: degToRad(0),
+  wallThickness: 2,
+  segments: 64,
+};
+
+config.connector1Modifier = (object) => {
+  const plateWidth = config.connector1.outerDiameterA + config.wallThickness + 20;
+  const holeDistance = 2 * config.wallThickness;
+  const holeDiameter = 3;
+  return union(object, mounts.plateRectangular({ size: plateWidth, distance: holeDistance, radius: holeDiameter / 2 }, object));
+};
+
+module.exports = require('../../../lib/submodel').exports(require('../suction-hose-adapter'), config);
